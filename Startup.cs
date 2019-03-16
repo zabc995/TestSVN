ab574a59455a2a7d152860137dda2f8bf78934a4
zabc995 <32014883+zabc995@users.noreply.github.com>
2019-03-16 14:33:28 +0700

    HAIZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSVN.Startup))]
+namespace WebSVN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
